865f502b50c9ba20e458647bd94114abba1d5e35
MercyMwende-collab <mwendemercy018@gmail.com>
2026-06-11 21:08:51 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,15 @@
+print("Hello! I am becoming an ML Engineer")
+print("I am learning Python")
+name = "Mercy"
+age = 18
+print(name)
+print(age)
+name = input ("what is your name? ")
+age = input ("how old are you? ")
+print("Hello, " + name + " you are " + age + " years old!")
+first_name = input("what is your first name? ")
+last_name = input("what is your last name? ")
+full_name = first_name + " " + last_name
+course = input ("what course are you taking? ")
+university = input ("Which university? ")
+print ( "My name is  " + full_name + " I study " + course + " at " + university + " and I want to be a ML Engineer.")
