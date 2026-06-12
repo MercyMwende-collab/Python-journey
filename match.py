1d794843e8c6dd3b7c2564fea0b7e0fcb28d37bb
MercyMwende-collab <mwendemercy018@gmail.com>
2026-06-13 00:55:20 +0300

    Add files via upload

---
+++ match.py
@@ -0,0 +1,18 @@
+name = input("What is your name? ") 
+
+match name :
+    case "Mercy" | "Patience" | "Danielle":
+        print ("Qejani")
+    case "Bill":
+        print ("Qwetu")
+    case _ :
+        print ("Who?")
+
+name =input("What is your name? ")
+        
+if name == "Mercy" or name == "Patience" or name == "Danielle": 
+     print ("Qejani")
+elif name == "Bill":
+    print ("Qwetu")
+else:    print ("Who?") 
+
